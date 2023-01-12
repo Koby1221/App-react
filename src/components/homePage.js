a38687b7314c9d98b37b6f315b8449c2f07fe302
Kobi <121023399+Koby1221@users.noreply.github.com>
2023-01-12 13:12:31 +0200

    s

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -69,19 +69,32 @@ const HomePage= (props)=>{
 
 						// 
 						
-
-
-						<div>
-						<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
-						<h1>Card Image with description on :hover</h1>
-						<div class="cards">
-  						<div class="card">
-    					<h2 class="card-title">Seal</h2>
-    					< img src="https://images.unsplash.com/photo-1591485423007-765bde4139ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
-    					<p class="card-desc">Pinnipeds, commonly known as seals,[a] are a widely distributed and diverse clade of carnivorous, fin-footed, semiaquatic marine mammals. They comprise the extant families Odobenidae (whose only living member is the walrus), Otariidae (the eared seals: sea lions and fur seals), and Phocidae (the earless seals, or true seals).</p>
-  						</div>
-  						</div>
+						
+						// <div>
+						// <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
+						// <h1>Card Image with description on :hover</h1>
+						// <div class="cards">
+  						// <div class="card">
+    					// <h2 class="card-title">Seal</h2>
+    					// < img src="https://images.unsplash.com/photo-1591485423007-765bde4139ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+    					// <p class="card-desc">Pinnipeds, commonly known as seals,[a] are a widely distributed and diverse clade of carnivorous, fin-footed, semiaquatic marine mammals. They comprise the extant families Odobenidae (whose only living member is the walrus), Otariidae (the eared seals: sea lions and fur seals), and Phocidae (the earless seals, or true seals).</p>
+  						// </div>
+  						// </div>
+						// </div> <div className="col-lg-6  p-3" key={item.dt}>
+						
+						
+						<div className="p-2 shadow overflow-hidden" key={index}>
+						<h2> date : bffdbfdb</h2>
+						<div><h3 >temp :  vdsvdsvdsvs</h3> </div> 
+						<h2>description : fvdfsvdsvds</h2>
+						<img src={item.img} style={{width:"30%"}}></img>
 						</div>
+						 
+
+
+
+
+						
 						
 					)
 				}
